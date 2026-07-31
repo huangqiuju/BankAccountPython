256461b7c597cd0847120af90ed5938cc6fdcddd
Judy Huang <qiujuhuang@yahoo.com>
2026-07-30 22:42:35 -0700

    Added type annotations and changed members to private

--- src/bank/bank_account.py
+++ src/bank/bank_account.py
@@ -1,19 +1,21 @@
 class BankAccount:
-    def __init__(self, account_id, full_name, initial_balance):
+    def __init__(
+        self: "BankAccount", account_id: int, full_name: str, initial_balance: float
+    ):
         self._account_id = account_id
         self._full_name = full_name
         self._balance = initial_balance
 
-    def add_funds(self, amount):
+    def add_funds(self, amount: float):
         self._balance += amount
 
-    def withdraw_funds(self, amount):
+    def withdraw_funds(self, amount: float):
         self._balance -= amount
 
         if self._balance < 0:
             self._balance -= 20  # overdraft penalty
 
-    def print_info(self):
+    def print_info(self: "BankAccount"):
         print(f"Account ID: {self._account_id}")
         print(f"Full Name: {self._full_name}")
         print(f"Balance: ${self._balance}")
--- src/bank/gold_account.py
+++ src/bank/gold_account.py
@@ -2,5 +2,5 @@ from bank.bank_account import BankAccount
 
 
 class GoldAccount(BankAccount):
-    def withdraw_funds(self, amount):
+    def withdraw_funds(self: "GoldAccount", amount: float):
         self._balance -= amount  # no overdraft penalty
