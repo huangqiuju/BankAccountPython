4e19821f7ca27f0bdc7cb127e335ad4a6efbb39f
Judy Huang <qiujuhuang@yahoo.com>
2026-07-30 22:50:12 -0700

    Add return type annotation.

--- src/bank/bank_account.py
+++ src/bank/bank_account.py
@@ -1,21 +1,21 @@
 class BankAccount:
     def __init__(
         self: "BankAccount", account_id: int, full_name: str, initial_balance: float
-    ):
+    ) -> None:
         self._account_id = account_id
         self._full_name = full_name
         self._balance = initial_balance
 
-    def add_funds(self, amount: float):
+    def add_funds(self: "BankAccount", amount: float) -> None:
         self._balance += amount
 
-    def withdraw_funds(self, amount: float):
+    def withdraw_funds(self: "BankAccount", amount: float) -> None:
         self._balance -= amount
 
         if self._balance < 0:
             self._balance -= 20  # overdraft penalty
 
-    def print_info(self: "BankAccount"):
+    def print_info(self: "BankAccount") -> None:
         print(f"Account ID: {self._account_id}")
         print(f"Full Name: {self._full_name}")
         print(f"Balance: ${self._balance}")
--- src/bank/gold_account.py
+++ src/bank/gold_account.py
@@ -2,5 +2,7 @@ from bank.bank_account import BankAccount
 
 
 class GoldAccount(BankAccount):
-    def withdraw_funds(self: "GoldAccount", amount: float):
+    def withdraw_funds(self: "GoldAccount", amount: float) -> None:
         self._balance -= amount  # no overdraft penalty
+
+        return None
